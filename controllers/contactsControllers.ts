210ec56c15cc45996fc5f4874a3600dfcd44e353
Taras Kovalyk <tarik2454@gmail.com>
2024-12-03 22:04:27 +0200

    start project

---
+++ controllers/contactsControllers.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+export const getAllContacts = (req: Request, res: Response) => {
+  // ваш код
+};
+
+export const getOneContact = (req: Request, res: Response) => {
+  // ваш код
+};
+
+export const deleteContact = (req: Request, res: Response) => {
+  // ваш код
+};
+
+export const createContact = (req: Request, res: Response) => {
+  // ваш код
+};
+
+export const updateContact = (req: Request, res: Response) => {
+  // ваш код
+};
---
+++ dist/controllers/contactsControllers.js
@@ -0,0 +1,15 @@
+export const getAllContacts = (req, res) => {
+    // ваш код
+};
+export const getOneContact = (req, res) => {
+    // ваш код
+};
+export const deleteContact = (req, res) => {
+    // ваш код
+};
+export const createContact = (req, res) => {
+    // ваш код
+};
+export const updateContact = (req, res) => {
+    // ваш код
+};
