03b9d13a2cd85225b10ca07c7af1ed5670935a09
Taras Kovalyk <tarik2454@gmail.com>
2024-12-04 21:45:05 +0200

    adds the creation of a basic file structure

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
-import contactsRouter from './routes/contactsRouter.js';
+import eventsRouter from './routes/eventsRouter.js';
 
 interface HttpError extends Error {
   status: number;
@@ -14,7 +14,9 @@ app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+app.use('/api/contacts', eventsRouter);
+
+console.log(eventsRouter);
 
 app.use((_: Request, res: Response) => {
   res.status(404).json({ message: 'Route not found' });
---
+++ controllers/eventsControllers.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+export const getAllEvents = (req: Request, res: Response) => {
+  // ваш код
+};
+
+export const getOneEvent = (req: Request, res: Response) => {
+  // ваш код
+};
+
+export const deleteEvent = (req: Request, res: Response) => {
+  // ваш код
+};
+
+export const createEvent = (req: Request, res: Response) => {
+  // ваш код
+};
+
+export const updateEvent = (req: Request, res: Response) => {
+  // ваш код
+};
