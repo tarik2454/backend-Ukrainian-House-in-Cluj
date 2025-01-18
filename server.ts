02cd3d5b0cf486478c96f85f24b658db89485b06
Taras Kovalyk <tarik2454@gmail.com>
2025-01-18 19:31:35 +0200

    adds mongoDB base

--- server.ts
+++ server.ts
@@ -1,5 +1,18 @@
+import mongoose from 'mongoose';
+
 import app from './app.ts';
 
-app.listen(3001, () => {
-  console.log('Server is running. Use our API on port: 3001');
-});
+const DB_HOST =
+  'mongodb+srv://tarik2454:4tNelyySmiuNs9aS@cluster0.pz8jo.mongodb.net/backend-Ukrainian-House-in-Cluj?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3001, () => {
+      console.log('Server is running. Use our API on port: 3001');
+    });
+  })
+  .catch(err => {
+    console.log(err.message);
+    process.exit(1);
+  });
