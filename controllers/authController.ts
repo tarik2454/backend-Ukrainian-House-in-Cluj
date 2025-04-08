9299f43c810daa33e826958abf2f93749f8ec5a6
Taras Kovalyk <tarik2454@gmail.com>
2025-04-08 12:48:54 +0300

    fix: update import path for sendEmail helper in authController

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -10,7 +10,7 @@ import { nanoid } from 'nanoid';
 import User from '../models/User';
 import HttpError from '../helpers/HttpError';
 import ctrlWrapper from '../decorators/ctrlWrapper';
-import sendEmail from '@/helpers/sendEmail';
+import sendEmail from '../helpers/sendEmail';
 
 interface AuthRequest extends Request {
   user?: { _id: string; username?: string; email?: string };
